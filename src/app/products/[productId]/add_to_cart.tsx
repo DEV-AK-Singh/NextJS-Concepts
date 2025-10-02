e86c663bc19ce885931f2b3f2d5ecd981fb5bdda
Abhishek Singh <singh.abhishek151019@gmail.com>
2025-10-02 17:12:30 +0530

    done navigating concept

--- src/app/products/[productId]/add_to_cart.tsx
+++ src/app/products/[productId]/add_to_cart.tsx
@@ -1,8 +1,31 @@
 "use client";
 
+import { redirect, useRouter } from "next/navigation";
 import React from "react";
 
-export default function AddCart() {    
+
+export default function AddCart() {
+  const router = useRouter();
   const [count, setCount] = React.useState(0);
-  return <button onClick={() => setCount(count + 1)} className="bg-blue-500 hover:bg-blue-700 text-white font-bold py-1 px-4 rounded-2xl">Add to Cart + {count}</button>;
+  const Navigate = () => {
+    // router.push("/"); 
+    // router.replace("/");
+    redirect("/");
+  };
+  return (
+    <>
+      <button
+        onClick={() => setCount(count + 1)}
+        className="bg-blue-500 hover:bg-blue-700 text-white font-bold py-1 px-4 rounded-2xl"
+      >
+        Add to Cart + {count}
+      </button>
+      <button 
+        onClick={Navigate}
+        className="bg-green-500 hover:bg-green-700 text-white font-bold py-1 px-4 rounded-2xl ms-2"
+      >
+        place order
+      </button>
+    </>
+  );
 }
