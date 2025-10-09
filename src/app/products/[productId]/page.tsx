a9d8462551f45a16c4c2af20842eb14597a14100
Abhishek Singh <singh.abhishek151019@gmail.com>
2025-10-10 00:03:51 +0530

    done rendering concept (dynamic and static)

--- src/app/products/[productId]/page.tsx
+++ src/app/products/[productId]/page.tsx
@@ -2,6 +2,10 @@ import { Metadata } from "next";
 import React from "react";
 import AddCart from "./add_to_cart";
 
+export const dynamicParams = true;
+
+export const generateStaticParams = () => [{ productId: "1" }, { productId: "2" }, { productId: "3" }]; 
+
 export const generateMetadata = async ({ params }: { params: Promise<{ productId: string }> }) : Promise<Metadata> => {
   const { productId } = await params;
   const title = await new Promise((resolve) => setTimeout(() => resolve(`Next.js`), 1000)); 
