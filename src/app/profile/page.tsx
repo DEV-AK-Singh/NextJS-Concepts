c79b1d0e37299ae5cb41385591d8ec4f78d5f296
Abhishek Singh <singh.abhishek151019@gmail.com>
2025-10-10 16:23:13 +0530

    done server only code concept

--- src/app/profile/page.tsx
+++ src/app/profile/page.tsx
@@ -1,5 +1,9 @@
+"use client";
+
 import React from "react";
+// import { serverUtils } from "../utils/server-utils";
 
 export default function Profile() {
+  // console.log(serverUtils());
   return <div>Profile</div>;
 }
---
+++ src/app/utils/server-utils.ts
@@ -0,0 +1,6 @@
+import "server-only";
+
+export const serverUtils = () => {
+    console.log("server utils");
+    return "server utils";
+}
